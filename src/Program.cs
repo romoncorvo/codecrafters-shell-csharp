39aca456161e17e86c42384912fef6e3b16d27b5
romoncorvo <36642718+romoncorvo@users.noreply.github.com>
2024-12-01 20:57:46 +0100

    The exit builtin

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,28 @@
 internal static class Program
 {
-    public static void Main(string[] args)
+    public static int Main(string[] args)
     {
         while (true)
         {
             Console.Write("$ ");
 
-            var command = Console.ReadLine();
-            Console.WriteLine($"{command}: command not found");
+            var input = Console.ReadLine();
+
+            if (input != null)
+            {
+                var inputs = input.Split(' ');
+                var command = inputs[0];
+
+                if (command == "exit")
+                {
+                    if (inputs.Length > 1 && int.TryParse(inputs[1], out var n) && n is >= 0 and <= 255)
+                        return n;
+
+                    return 0;
+                }
+
+                Console.WriteLine($"{input}: command not found");
+            }
         }
     }
 }
